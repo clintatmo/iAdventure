aaae13e5cfa730ddfe0877335694d16095dddd47
qualogy-mac <clintatmo@gmail.com>
2017-03-09 14:52:55 -0300

    added  fragments with onclick listeners

--- app/src/main/java/catmosoerodjo/sr/iadventure/NewAdventureFragment.java
+++ app/src/main/java/catmosoerodjo/sr/iadventure/NewAdventureFragment.java
@@ -3,6 +3,8 @@ package catmosoerodjo.sr.iadventure;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,8 +65,20 @@ public class NewAdventureFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_new_adventure, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_adventure, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
